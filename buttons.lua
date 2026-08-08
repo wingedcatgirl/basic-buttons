20edb7e9e628e13c813ed8b7225d3e3550020cea
wingedcatgirl <69098783+wingedcatgirl@users.noreply.github.com>
2026-08-07 22:44:50 -0400

    here actually

--- buttons.lua
+++ buttons.lua
@@ -2,7 +2,7 @@
 -- Modified code from https://github.com/Foo54/SynthB/blob/main/src/api/useable_joker.lua
 -- which in turn modified it from spectrallib https://github.com/SpectralPack/Spectrallib/blob/main/Entropy/card_buttons.lua ahahaha there is so much going on in here i am NOT touching that
 --
--- This specific iteration is from https://github.com/wingedcatgirl/basic-buttons
+-- This specific iteration is from https://github.com/wingedcatgirl/basic-buttons, you can check the version number in the mod's metadata
 -- It is intended to be copied into other Balatro mods in its entirety, but please leave these lines in so people can check for an updated version
 
 local myprefix = SMODS.current_mod.prefix
